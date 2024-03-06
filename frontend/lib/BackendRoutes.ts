e95dbf7370f8da4503ee0b925abc2577ad5df6ab
Nils Reiter <nils.reiter@uni-koeln.de>
2024-03-06 14:13:06 +0100

    Backend base url needs to be different, because it's wrapped by nginx

--- frontend/lib/BackendRoutes.ts
+++ frontend/lib/BackendRoutes.ts
@@ -1,5 +1,5 @@
 class BACKENDROUTES {
-    private static BASE_URL = `http://localhost:8080`;
+    private static BASE_URL = `/phitag-backend`;
     private static API_VERSION = `/api/v1`;
 
     public static AUTHENTICATION = `${BACKENDROUTES.BASE_URL}${BACKENDROUTES.API_VERSION}/authentication`;
